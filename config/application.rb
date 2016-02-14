e59a4a3a4389271624a3f05e7b03bf639aa183cd
felixeroidis@hotmail.com <felixeroidis@hotmail.com>
2016-02-14 18:12:24 -0500

    To enable assets pipiline

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,5 +23,7 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # to enable assets pipeline
+    config.serve_static_assets = true
   end
 end
